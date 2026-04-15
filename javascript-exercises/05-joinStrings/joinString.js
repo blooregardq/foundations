a2ad564d9d1cdea956ed6c87b9be7d80ad0dbcd3
bloo <88352352+blooregardq@users.noreply.github.com>
2026-04-14 21:56:31 -0500

    npm testing with strings

--- javascript-exercises/05-joinStrings/joinString.js
+++ javascript-exercises/05-joinStrings/joinString.js
@@ -1,3 +1,10 @@
-const firstName = joe 
-const lastName = doe 
-const thisYear = 2026
+const firstName = "Joe";
+const lastName = "Doe";
+const thisYear = 2026;
+const birthYear = 1999;
+
+const fullName = firstName + " " + lastName;
+
+const age = thisYear - birthYear;
+
+const greeting = " hello! my name is " + fullName + "and i am " + age + "years old. ";
